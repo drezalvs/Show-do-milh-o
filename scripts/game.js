c27360517e7e68ecfa8342797bf0b04cb0a4da55
Andreza Alves <drezlves@gmail.com>
2020-07-06 16:34:07 -0300

    Criando o layout

---
+++ scripts/game.js
@@ -0,0 +1,3 @@
+var gabarito = new Array(6);
+var gabarito = new Array('D', 'A', 'A', 'D', 'C', 'B');
+
---
+++ scripts/modalbvindo.js
@@ -0,0 +1,4 @@
+function bemvindo() {
+    var nome =  window.prompt(`Olá! Qual é o seu nome?`)
+    alert(`Olá, ${nome}. Seja bem-vinda(o) ao Show do Milhão! Venha jogar!".`)
+}
