7b4e412704814ce7466622d7c988b85995b9c697
Andreza Alves <drezlves@gmail.com>
2020-07-07 20:07:25 -0300

    Update modalbvindo.js

--- scripts/modalbvindo.js
+++ scripts/modalbvindo.js
@@ -1,4 +1,4 @@
 function bemvindo() {
     var nome =  window.prompt(`Olá! Qual é o seu nome?`)
-    alert(`Olá, ${nome}. Seja bem-vinda(o) ao Show do Milhão! Venha jogar!".`)
+    alert(`Olá, ${nome}. Seja bem-vinda(o) ao Show do Milhão! Vamos jogar!".`)
 }
